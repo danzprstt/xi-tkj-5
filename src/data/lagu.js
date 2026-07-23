470327528301a0d4e5c57d9a5466ef3a4ed8563d
danz-dev <gamelegend1092@gmail.com>
2026-07-23 09:31:57 +0700

    Update lagu.js

--- src/data/lagu.js
+++ src/data/lagu.js
@@ -1,40 +1,40 @@
 export const lagu = {
-  1:  { link: '/lagu/1.mp3',  pencipta: '', judul: '', keterangan: '' }, // Adha Vanrizal
-  2:  { link: '/lagu/2.mp3',  pencipta: '', judul: '', keterangan: '' }, // Afrand Al Gheysar
-  3:  { link: '/lagu/agus.mp3',  pencipta: 'Oasis', judul: 'Whatever', keterangan: 'Britpop klasik era 90-an yang megah, ceria, dan penuh kebebasan.' }, // Agus Setiawan
-  4:  { link: '/lagu/4.mp3',  pencipta: '', judul: '', keterangan: '' }, // Alsyifa Annatul Fatwa
-  5:  { link: '/lagu/anisa.mp3',  pencipta: 'Daniel Caesar', judul: 'Hold Me Down', keterangan: 'R&B/Soul yang lambat, intim, groovy, dan sangat kental dengan nuansa musik gereja/gospel modern.' }, // Anisa Ramadani
-  6:  { link: '/lagu/6.mp3',  pencipta: '', judul: '', keterangan: '' }, // Ariffa Cahyan Putra
-  7:  { link: '/lagu/audia.mp3',  pencipta: 'LANY', judul: 'DNA', keterangan: 'Indie-pop elektronik modern yang manis, bucin, dan easy listening.' }, // Audia Safira
-  8:  { link: '/lagu/aura.mp3',  pencipta: 'Daniel Caesar', judul: 'Transform', keterangan: 'R&B/Soul yang super smooth, intim, romantis, dan sangat rileks.' }, // Aura Latif Putri Utomo
-  9:  { link: '/lagu/ika.mp3',  pencipta: 'Bon Jovi', judul: 'Always', keterangan: 'Power ballad rock klasik yang super dramatis, megah, dan emosional.' }, // Cantika Diva Pratama
-  10: { link: '/lagu/dea.mp3', pencipta: 'Raim Laode', judul: 'Dunia Yang Nanti', keterangan: 'Pop akustik yang puitis, tulus, dan penuh keikhlasan tentang rasa cinta.' }, // Dea Ananda
-  11: { link: '/lagu/dewi.mp3', pencipta: 'Taylor Swift', judul: 'White Horse', keterangan: 'Country-pop melankolis tentang kekecewaan, patah hati, dan momen tersadar dari ekspektasi yang terlalu tinggi.' }, // Dewi Ayyudya Nagasari
-  12: { link: '/lagu/dian.mp3', pencipta: 'Daniel Caesar', judul: 'Get You', keterangan: 'R&B/Soul yang super smooth, intim, romantis, dan sangat rileks.' }, // Dian Rahayu
-  13: { link: '/lagu/13.mp3', pencipta: '', judul: '', keterangan: '' }, // Elis
-  14: { link: '/lagu/14.mp3', pencipta: '', judul: '', keterangan: '' }, // Haykal Syahmaludin Kamil
-  15: { link: '/lagu/isan.mp3', pencipta: 'Natori', judul: 'Propose', keterangan: 'J-Pop modern yang catchy, punya groove yang unik, dan lirik yang manis tapi santai.' }, // Ihsan Zamir Juhanto
-  16: { link: '/lagu/kania.mp3', pencipta: 'Raim Laode', judul: 'Dunia yang Nanti', keterangan: 'Pop akustik lokal yang puitis dan penuh keikhlasan tentang cinta yang tak harus memiliki.' }, // Kania Waliani
-  17: { link: '/lagu/karlin.mp3', pencipta: 'Raim Laode', judul: 'Iqro', keterangan: 'Musik folk-pop kontemplatif yang religius, penuh rasa syukur, dan refleksi kehidupan.' }, // Karline Widy Wijaya
-  18: { link: '/lagu/itos.mp3', pencipta: 'The Jansen', judul: 'Berkelana Dalam Ruang dan Mimpi', keterangan: 'Pop-punk/indie-rock analog dengan sound mentah khas tahun 70-an/80-an. Ketukannya cepat, dinamis, tapi liriknya membawa nuansa khayalan dan kebebasan berpikir.' }, // Karlytos Daniel
-  19: { link: '/lagu/19.mp3', pencipta: '', judul: '', keterangan: '' }, // Maliki Damar Al Mughni
-  20: { link: '/lagu/meli.mp3', pencipta: 'For Revenge', judul: 'Penyangkalan', keterangan: 'Emo/Modern rock lokal yang meledak-ledak, penuh distorsi gitar, dan lirik patah hati yang sangat perih.' }, // Melly Agustin
-  21: { link: '/lagu/idan.mp3', pencipta: "Backstreet Boys", judul: "Shape of My Heart", keterangan: "Pop ballad era 2000-an awal yang megah, romantis, dan ppenuh penyesalan serta permintaan maaf dari lubuk hati." }, // Muhammad Dias Wildan Adam
-  22: { link: '/lagu/22.mp3', pencipta: '', judul: '', keterangan: '' }, // Muhammad Khadafi
-  23: { link: '/lagu/23.mp3', pencipta: '', judul: '', keterangan: '' }, // Muhammad Rafif Ar Raqi
-  24: { link: '/lagu/rafa.mp3', pencipta: 'Justin Bieber', judul: 'Flatline', keterangan: 'R&B/Pop pertengahan 2010-an yang catchy, emosional, dan berfokus pada rasa rindu serta komunikasi yang terputus.' }, // Muhammad Rava Alrosyid
-  25: { link: '/lagu/sarul.mp3', pencipta: 'Yuta (OST Kamen Rider Geats)', judul: 'Play Back', keterangan: 'J-Rock/J-Pop yang upbeat, energik, penuh semangat pahlawan, dan memacu adrenalin.' }, // Muhammad Syahrul Ramadhan
-  26: { link: '/lagu/nafi.mp3', pencipta: 'Olivia Rodrigo', judul: 'Drop Dead', keterangan: 'Pop-rock/punk remaja yang sarkastik, edgy, penuh kemarahan yang stylish, dan ekspresif.' }, // Nafisatu Rohmah
-  27: { link: '/lagu/nurul.mp3', pencipta: 'Tulus', judul: 'Teh Hijau', keterangan: 'Jazz-pop/indie pop lokal yang ringan, segar, unik, dan penuh dengan analogi kehidupan yang cerdas.' }, // Nurul Latifah
-  28: { link: '/lagu/28.mp3', pencipta: '', judul: '', keterangan: '' }, // Rafael Dwi Andika
-  29: { link: '/lagu/29.mp3', pencipta: '', judul: '', keterangan: '' }, // Raffa Azzam Hafifi
-  30: { link: '/lagu/30.mp3', pencipta: '', judul: '', keterangan: '' }, // Rico Adhytia
-  31: { link: '/lagu/fahmi.mp3', pencipta: 'Threesixty', judul: 'Dewi', keterangan: 'Melodic punk lokal yang bertenaga, cepat, tapi punya lirik yang emosional dan romantis dengan gaya jalanan.' }, // Rizallul Fahmi
-  32: { link: '/lagu/32.mp3', pencipta: '', judul: '', keterangan: '' }, // Rizqy Aidhil Rachman
-  33: { link: '/lagu/salwa.mp3', pencipta: 'Hoobastank', judul: 'The Reason', keterangan: 'Alternatif rock awal 2000-an yang emosional dan penuh penyesalan sekaligus refleksi diri.' }, // Salwa Aisyah Azri
-  34: { link: '/lagu/sela.mp3', pencipta: 'Dewa 19', judul: 'Pupus', keterangan: 'Pop-rock Indonesia legendaris yang megah, tragis, dan menjadi anthem patah hati nasional bergenerasi-generasi.' }, // Shela Lestari
-  35: { link: '/lagu/soba.mp3', pencipta: 'Trevor Daniel & Ariana Grande', judul: 'Past Life', keterangan: 'Pop R&B yang dreamy, modern, dan agak melankolis.' }, // Sobarina Al Maqhviro
-  36: { link: '/lagu/suci.mp3', pencipta: 'Khai Dreams', judul: 'Sunkissed', keterangan: 'Lo-fi indie pop yang sangat santai, ceria, hangat, dan penuh nuansa musim panas.' }, // Suci Wulandari
-  37: { link: '/lagu/37.mp3', pencipta: '', judul: '', keterangan: '' }, // Uswatun Hasanah Oktaviani
-  38: { link: '/lagu/nita.mp3', pencipta: 'Kaleb J', judul: 'Kebutuhan Hati', keterangan: 'R&B pop lokal yang galau, menyayat hati, tapi tetap terdengar smooth dan nyaman di telinga.' }  // Yunita
+  1:  { link: './lagu/ada.mp3',  pencipta: 'Tiara Andini', judul: 'Tanpa Cinta', keterangan: 'Pop ballad Indonesia yang emosional, megah, dan menyayat hati. Vokal Tiara Andini yang kuat membawa nuansa ketegasan di tengah rasa sakit karena harus melepaskan seseorang yang sudah tidak lagi memberikan cinta.' }, // Adha Vanrizal
+  2:  { link: './lagu/2.mp3',  pencipta: '', judul: '', keterangan: '' }, // Afrand Al Gheysar
+  3:  { link: './lagu/agus.mp3',  pencipta: 'Oasis', judul: 'Whatever', keterangan: 'Britpop klasik era 90-an yang megah, ceria, dan penuh kebebasan.' }, // Agus Setiawan
+  4:  { link: './lagu/sipa.mp3',  pencipta: 'Taylor Swift', judul: 'loml', keterangan: 'Piano ballad yang sangat melankolis, puitis, dan penuh rasa kehilangan. Lagu ini menceritakan tentang seseorang yang awalnya dianggap sebagai love of my life (cinta sehidup semati), namun akhirnya berubah menjadi loss of my life (kehilangan terbesar dalam hidup).' }, // Alsyifa Annatul Fatwa
+  5:  { link: './lagu/anisa.mp3',  pencipta: 'Daniel Caesar', judul: 'Hold Me Down', keterangan: 'R&B/Soul yang lambat, intim, groovy, dan sangat kental dengan nuansa musik gereja/gospel modern.' }, // Anisa Ramadani
+  6:  { link: './lagu/6.mp3',  pencipta: '', judul: '', keterangan: '' }, // Ariffa Cahyan Putra
+  7:  { link: './lagu/audia.mp3',  pencipta: 'LANY', judul: 'DNA', keterangan: 'Indie-pop elektronik modern yang manis, bucin, dan easy listening.' }, // Audia Safira
+  8:  { link: './lagu/aura.mp3',  pencipta: 'Daniel Caesar', judul: 'Transform', keterangan: 'R&B/Soul yang super smooth, intim, romantis, dan sangat rileks.' }, // Aura Latif Putri Utomo
+  9:  { link: './lagu/ika.mp3',  pencipta: 'Bon Jovi', judul: 'Always', keterangan: 'Power ballad rock klasik yang super dramatis, megah, dan emosional.' }, // Cantika Diva Pratama
+  10: { link: './lagu/dea.mp3', pencipta: 'Raim Laode', judul: 'Dunia Yang Nanti', keterangan: 'Pop akustik yang puitis, tulus, dan penuh keikhlasan tentang rasa cinta.' }, // Dea Ananda
+  11: { link: './lagu/dewi.mp3', pencipta: 'Taylor Swift', judul: 'White Horse', keterangan: 'Country-pop melankolis tentang kekecewaan, patah hati, dan momen tersadar dari ekspektasi yang terlalu tinggi.' }, // Dewi Ayyudya Nagasari
+  12: { link: './lagu/dian.mp3', pencipta: 'Daniel Caesar', judul: 'Get You', keterangan: 'R&B/Soul yang super smooth, intim, romantis, dan sangat rileks.' }, // Dian Rahayu
+  13: { link: './lagu/elis.mp3', pencipta: 'NU', judul: 'NU', keterangan: 'Breakbeat nostalgia ala clubbing/stadium awal 2000-an. Dentuman bassline yang padat, tempo ajeg, dan groove elektro-klasik yang bikin otomatis kejut bahu.' }, // Elis
+  14: { link: './lagu/haykal.mp3', pencipta: 'Troye Sivan', judul: 'Angel Baby', keterangan: 'Pop ballad synth-wave era 80-an yang dreamy, romantis, dan penuh rasa kasmaran. Melodinya megah tapi tetap kerasa lembut dan bikin fly.' }, // Haykal Syahmaludin Kamil
+  15: { link: './lagu/isan.mp3', pencipta: 'Natori', judul: 'Propose', keterangan: 'J-Pop modern yang catchy, punya groove yang unik, dan lirik yang manis tapi santai.' }, // Ihsan Zamir Juhanto
+  16: { link: './lagu/kania.mp3', pencipta: 'Raim Laode', judul: 'Dunia yang Nanti', keterangan: 'Pop akustik lokal yang puitis dan penuh keikhlasan tentang cinta yang tak harus memiliki.' }, // Kania Waliani
+  17: { link: './lagu/karlin.mp3', pencipta: 'Raim Laode', judul: 'Iqro', keterangan: 'Musik folk-pop kontemplatif yang religius, penuh rasa syukur, dan refleksi kehidupan.' }, // Karline Widy Wijaya
+  18: { link: './lagu/itos.mp3', pencipta: 'The Jansen', judul: 'Berkelana Dalam Ruang dan Mimpi', keterangan: 'Pop-punk/indie-rock analog dengan sound mentah khas tahun 70-an/80-an. Ketukannya cepat, dinamis, tapi liriknya membawa nuansa khayalan dan kebebasan berpikir.' }, // Karlytos Daniel
+  19: { link: './lagu/19.mp3', pencipta: '', judul: '', keterangan: '' }, // Maliki Damar Al Mughni
+  20: { link: './lagu/meli.mp3', pencipta: 'For Revenge', judul: 'Penyangkalan', keterangan: 'Emo/Modern rock lokal yang meledak-ledak, penuh distorsi gitar, dan lirik patah hati yang sangat perih.' }, // Melly Agustin
+  21: { link: './lagu/idan.mp3', pencipta: 'Taylor Swift', judul: 'Megamix', keterangan: 'Energik, seru, dan full-power! Perpaduan dari berbagai era musik Taylor Swift—dari era country awal, pop ceria, hingga synth-pop yang dramatis—digabung jadi satu trek yang dinamis dan gak ngebosenin.' }, // Muhammad Dias Wildan Adam
+  22: { link: './lagu/22.mp3', pencipta: '', judul: '', keterangan: '' }, // Muhammad Khadafi
+  23: { link: './lagu/23.mp3', pencipta: '', judul: '', keterangan: '' }, // Muhammad Rafif Ar Raqi
+  24: { link: './lagu/rafa.mp3', pencipta: 'Justin Bieber', judul: 'Flatline', keterangan: 'R&B/Pop pertengahan 2010-an yang catchy, emosional, dan berfokus pada rasa rindu serta komunikasi yang terputus.' }, // Muhammad Rava Alrosyid
+  25: { link: './lagu/sarul.mp3', pencipta: 'Yuta (OST Kamen Rider Geats)', judul: 'Play Back', keterangan: 'J-Rock/J-Pop yang upbeat, energik, penuh semangat pahlawan, dan memacu adrenalin.' }, // Muhammad Syahrul Ramadhan
+  26: { link: './lagu/nafi.mp3', pencipta: 'Olivia Rodrigo', judul: 'Drop Dead', keterangan: 'Pop-rock/punk remaja yang sarkastik, edgy, penuh kemarahan yang stylish, dan ekspresif.' }, // Nafisatu Rohmah
+  27: { link: './lagu/nurul.mp3', pencipta: 'Tulus', judul: 'Teh Hijau', keterangan: 'Jazz-pop/indie pop lokal yang ringan, segar, unik, dan penuh dengan analogi kehidupan yang cerdas.' }, // Nurul Latifah
+  28: { link: './lagu/pael.mp3', pencipta: 'vally.exe & SoundCirclet', judul: 'String Theory', keterangan: 'Electronic/Trance dengan sentuhan instrumen gesek (biola) yang intens, futuristik, dan dark-puitis. Sebagai fansong Honkai: Star Rail (karakter Herta), lagunya penuh nuansa eksperimen, boneka, dan pencarian jati diri di tengah ilusi.' }, // Rafael Dwi Andika
+  29: { link: './lagu/ajam.mp3', pencipta: 'Dave, Tems', judul: 'Raindance', keterangan: 'Beat Afro-R&B yang mellow bikin lagunya fleksibel—enak dibikin lagu pengantar tidur, tapi tetap asyik buat diputar di perjalanan malam hari.' }, // Raffa Azzam Hafifi
+  30: { link: './lagu/30.mp3', pencipta: '', judul: '', keterangan: '' }, // Rico Adhytia
+  31: { link: './lagu/fahmi.mp3', pencipta: 'Threesixty', judul: 'Dewi', keterangan: 'Melodic punk lokal yang bertenaga, cepat, tapi punya lirik yang emosional dan romantis dengan gaya jalanan.' }, // Rizallul Fahmi
+  32: { link: './lagu/aidil.mp3', pencipta: 'Beach Bunny', judul: 'Prompt Queen', keterangan: 'Indie pop/garage rock yang upbeat dan energik secara musik, tapi punya lirik yang puitis dan reflektif tentang standar kecantikan, tekanan sosial, dan penerimaan diri.' }, // Rizqy Aidhil Rachman
+  33: { link: './lagu/salwa.mp3', pencipta: 'Hoobastank', judul: 'The Reason', keterangan: 'Alternatif rock awal 2000-an yang emosional dan penuh penyesalan sekaligus refleksi diri.' }, // Salwa Aisyah Azri
+  34: { link: './lagu/sela.mp3', pencipta: 'Dewa 19', judul: 'Pupus', keterangan: 'Pop-rock Indonesia legendaris yang megah, tragis, dan menjadi anthem patah hati nasional bergenerasi-generasi.' }, // Shela Lestari
+  35: { link: './lagu/soba.mp3', pencipta: 'Trevor Daniel & Ariana Grande', judul: 'Past Life', keterangan: 'Pop R&B yang dreamy, modern, dan agak melankolis.' }, // Sobarina Al Maqhviro
+  36: { link: './lagu/suci.mp3', pencipta: 'Khai Dreams', judul: 'Sunkissed', keterangan: 'Lo-fi indie pop yang sangat santai, ceria, hangat, dan penuh nuansa musim panas.' }, // Suci Wulandari
+  37: { link: './lagu/37.mp3', pencipta: '', judul: '', keterangan: '' }, // Uswatun Hasanah Oktaviani
+  38: { link: './lagu/nita.mp3', pencipta: 'Kaleb J', judul: 'Kebutuhan Hati', keterangan: 'R&B pop lokal yang galau, menyayat hati, tapi tetap terdengar smooth dan nyaman di telinga.' }  // Yunita
 };
