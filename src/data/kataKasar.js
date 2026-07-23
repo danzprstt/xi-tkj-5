ede7e8cfa50b36dbfd79b4b896fc227723b2dea7
danz-dev <gamelegend1092@gmail.com>
2026-07-23 19:52:34 +0700

    Add new entries to KATA_KASAR array

--- src/data/kataKasar.js
+++ src/data/kataKasar.js
@@ -1,7 +1,7 @@
 export const KATA_KASAR = [
   'anjing','anjrit','anjay','njir','asu','goblok','goblog','tolol','bego','bangsat',
   'bajingan','jancok','jancuk','keparat','sialan','kampret','kunyuk','tai','taik',
-  'monyet','babi','bangke','bangkai','kontol','memek','pepek','pantek','ngentot',
+  'monyet','babi','bangke','bangkai','kontol','memek','pepek','pantek','ngentot', 'boty', 'BOTY', 'BOTI', 'b0ti', 'B0ty', 'bti', 'bty', 'bot1',
   'pecun','lonte','sundal','pelacur','kampang','kampangan','peler','titit','toket','gay','gayy','gaayy','gayyy','bowtie','boti','botii','booti','botiii','botiiii','#boti','#botii','#botiii',
   'pukimak','puki','kimak','laknat','ngewe','jembut','brengsek','biadab','dongo','celeng','ajg','birong','birong','birng','anjg','ajng','ktl','kontol','kontl','kntl','genjor','gnjor','gnjr','mmk','kampang','jamet','jmbt','jamt','jmt'
 ];
