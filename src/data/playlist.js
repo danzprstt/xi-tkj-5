c468151815b636592bebe5a30433c75663eeb29f
danz-dev <gamelegend1092@gmail.com>
2026-07-31 13:10:49 +0700

    Add files via upload

---
+++ src/data/playlist.js
@@ -0,0 +1,5 @@
+// Ganti dengan link playlist Spotify kelas kamu yang asli.
+// Format yang diterima: link playlist biasa dari Spotify, contoh:
+//   https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M
+// (buka playlist di app/web Spotify -> titik tiga -> Share -> Copy link to playlist)
+export const PLAYLIST_SPOTIFY_URL = 'https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M';
