82bf75f8fafb50dca5b72b592a6bf74f1efd2ac7
danz-dev <gamelegend1092@gmail.com>
2026-07-31 13:24:39 +0700

    Update Spotify playlist URL in playlist.js

--- src/data/playlist.js
+++ src/data/playlist.js
@@ -2,4 +2,5 @@
 // Format yang diterima: link playlist biasa dari Spotify, contoh:
 //   https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M
 // (buka playlist di app/web Spotify -> titik tiga -> Share -> Copy link to playlist)
-export const PLAYLIST_SPOTIFY_URL = 'https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M';
+/*export const PLAYLIST_SPOTIFY_URL = 'https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M';*/
+export const PLAYLIST_SPOTIFY_URL = 'https://open.spotify.com/playlist/0qH9aNYvcxa6qCIgH2AkBY';
